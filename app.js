628fb37b13acbfa45faacd4d5d803427b6462071
danis94 <dcanadablandon@gmail.com>
2020-07-22 09:52:39 -0600

    Este commit es algo asi como de prueba

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/blogApp",{ useNewUrlParser: true,useUnifiedTopology: true });
 const _ = require("lodash");
 //path node's module The Path module provides a way of working with directories and file paths.
 //var path = require('path');
 const content = require(__dirname + "/content.js");
 const app = express();
 
-const articles = [];
+//First create a Schema 
+const  articlesSchema = new mongoose.Schema(
+    {
+        title:{
+            type: String,
+            required: true
+        },
+        body:{
+            type: String,
+            required: true
+        }
+    }
+);
+const ArticleModel = new mongoose.model("article",articlesSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine' , 'ejs');
 //app.set("views",path.join(__dirname,'views'));
 
-app.get("/",function(req,res){    
-    res.render('home',{content: content.homeContent,articles: articles});
+app.get("/",(req,res)=>{    
+    ArticleModel.find((err,articles)=>{
+        if(!err){
+            res.render('home',{content: content.homeContent,articles: articles});
+        }
+    });
 });
 
 app.get("/about",function(req,res){
@@ -31,34 +50,40 @@ app.get("/compose",function(req,res){
 });
 //routing parameters examples /articles/:articleTitle
 
-app.get("/show/:articleTitle",function(req,res){
-    let article = new content.articles();
-    article.title = _.lowerCase(req.params.articleTitle);
-    /* console.log(article); */
-
-    articles.every(function(element) {
-        //this variable result is used for store the value of element 'cause when lodash is used the value is changed
-        let result = element;
-        if (_.lowerCase(element.title) === article.title){
-            //this article = element is 'cause when the title matchs with element's title the element object is 
-            //put into article variable to send it back to the user 
-            article = result;
-            return false;  
-        } 
-        else return true
+app.get("/show/:articleId",function(req,res){
+    //article.title = _.lowerCase(req.params.articleTitle);
+    const articleName = req.params.articleTitle;
+    console.log(articleName);
+    ArticleModel.findOne({
+        id: req.params.articleTitle
+    },(err,result)=>{
+        //console.log(result);
+        if(!err){
+            res.render("showArticle",{result: result});
+        }
     });
-    res.render("showArticle",{article: article});
 });
 
 app.post("/compose",function(req,res){
-   let newArticle = new content.articles(req.body.articleTitle,req.body.articleBody);
-   //console.log(newArticle);
-   articles.push(newArticle);
-   res.redirect("/"); 
+    const title = _.lowerCase(req.body.articleTitle)
+    const body = req.body.articleBody;
+    //console.log(_.lowerCase(req.body.articleTitle));
+   let newArticle = new ArticleModel({
+       title: _.capitalize(title),
+       body: body
+    });
+   newArticle.save((err)=>{
+        if(!err){
+            res.redirect("/"); 
+        }else{
+            res.redirect("/compose");
+        }
+   });
 });
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
+  console.log("server running locally");
 }
 app.listen(port);
